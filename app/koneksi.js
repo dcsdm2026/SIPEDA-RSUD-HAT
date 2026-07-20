337d8b362b07ea49c099da79f2c5d827f5022938
dcsdm2026 <dc.sdm2026@gmail.com>
2026-07-20 10:33:04 +0700

    Update koneksi.js

--- app/koneksi.js
+++ app/koneksi.js
@@ -1,1 +1,8 @@
+// Memuat CDN Supabase pada JS: https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2
+const SUPABASE_URL = "https://ejvgembwkxgozpuuonpd.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVqdmdlbWJ3a3hnb3pwdXVvbnBkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ1MDQ3MzMsImV4cCI6MjEwMDA4MDczM30.V-QRzrmL4ZD9KELbdZcHLsnF2L87HuL9NTzfy-MDbqs";
 
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+// Export objek koneksi agar bisa dipanggil file lain
+window.db = supabase;
