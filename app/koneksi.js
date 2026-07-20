2c2c0a9e3422fda03c2f9a093e14ac0d11e1d635
dcsdm2026 <dc.sdm2026@gmail.com>
2026-07-20 11:45:57 +0700

    Update koneksi.js

--- app/koneksi.js
+++ app/koneksi.js
@@ -2,7 +2,7 @@
 // Konfigurasi Koneksi Supabase
 
 const SUPABASE_URL = "https://ejvgembwkxgozpuuonpd.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVqdmdlbWJ3a3hnb3pwdXVvbnBkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEyMzQwNzAsImV4cCI6MjA1NjgxMDA3MH0.T3vTbdF5zX090kI9P_GUpv9w_Vz2xV78u3E04V--FkE";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVqdmdlbWJ3a3hnb3pwdXVvbnBkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ1MDQ3MzMsImV4cCI6MjEwMDA4MDczM30.V-QRzrmL4ZD9KELbdZcHLsnF2L87HuL9NTzfy-MDbqs";
 
 // Inisialisasi Klien Supabase Global (Mencegah SyntaxError: Identifier already declared)
 if (typeof supabase !== 'undefined' && supabase.createClient) {
